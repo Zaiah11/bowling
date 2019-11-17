be130535e8ed86f01ba6935fc7a095a8bcec8c14
Isaiah Santala <Isantala@yahoo.com>
2019-11-16 16:55:59 -0800

    updated tests

--- test.js
+++ test.js
@@ -79,36 +79,27 @@ describe('Game', () => {
 
   it('should not allow adding after 10 rounds', () => {
     const newGame = new Game()
+
     newGame.addRoll(1) // 1
     newGame.addRoll(1)
-
     newGame.addRoll(1) // 2
     newGame.addRoll(1)
-
     newGame.addRoll(1) // 3
     newGame.addRoll(1)
-
     newGame.addRoll(1) // 4
     newGame.addRoll(1)
-
     newGame.addRoll(1) // 5
     newGame.addRoll(1)
-
     newGame.addRoll(1) // 6
     newGame.addRoll(1)
-
     newGame.addRoll(1) // 7
     newGame.addRoll(1)
-
     newGame.addRoll(1) // 8
     newGame.addRoll(1)
-
     newGame.addRoll(1) // 9
     newGame.addRoll(1)
-
     newGame.addRoll(1) // 10
     newGame.addRoll(1)
-
     newGame.addRoll(1) // 11
     newGame.addRoll(1)
 
@@ -118,38 +109,28 @@ describe('Game', () => {
 
   it('should handle strike at end of game', () => {
     const newGame = new Game()
+
     newGame.addRoll(1) // 1
     newGame.addRoll(1)
-
     newGame.addRoll(1) // 2
     newGame.addRoll(1)
-
     newGame.addRoll(1) // 3
     newGame.addRoll(1)
-
     newGame.addRoll(1) // 4
     newGame.addRoll(1)
-
     newGame.addRoll(1) // 5
     newGame.addRoll(1)
-
     newGame.addRoll(1) // 6
     newGame.addRoll(1)
-
     newGame.addRoll(1) // 7
     newGame.addRoll(1)
-
     newGame.addRoll(1) // 8
     newGame.addRoll(1)
-
     newGame.addRoll(1) // 9
     newGame.addRoll(1)
-
     newGame.addRoll(10) // 10
-
     newGame.addRoll(1) // 11
     newGame.addRoll(1)
-
     newGame.addRoll(1) // 12
     newGame.addRoll(1)
 
@@ -158,39 +139,79 @@ describe('Game', () => {
 
   it('should handle spare at end of game', () => {
     const newGame = new Game()
+
     newGame.addRoll(1) // 1
     newGame.addRoll(1)
-
     newGame.addRoll(1) // 2
     newGame.addRoll(1)
-
     newGame.addRoll(1) // 3
     newGame.addRoll(1)
-
     newGame.addRoll(1) // 4
     newGame.addRoll(1)
-
     newGame.addRoll(1) // 5
     newGame.addRoll(1)
-
     newGame.addRoll(1) // 6
     newGame.addRoll(1)
-
     newGame.addRoll(1) // 7
     newGame.addRoll(1)
-
     newGame.addRoll(1) // 8
     newGame.addRoll(1)
-
     newGame.addRoll(1) // 9
     newGame.addRoll(1)
-
     newGame.addRoll(5) // 10
     newGame.addRoll(5)
-
     newGame.addRoll(1) // 11
     newGame.addRoll(1)
 
     expect(newGame.total).to.equal(29)
+  })
+
+  it('should handle an entire game of strikes', () => {
+    const newGame = new Game()
+
+    newGame.addRoll(10) // 1
+    newGame.addRoll(10) // 2
+    newGame.addRoll(10) // 3
+    newGame.addRoll(10) // 4
+    newGame.addRoll(10) // 5
+    newGame.addRoll(10) // 6
+    newGame.addRoll(10) // 7
+    newGame.addRoll(10) // 8
+    newGame.addRoll(10) // 9
+    newGame.addRoll(10) // 10
+    newGame.addRoll(10) // 11
+    newGame.addRoll(10) // 12
+    newGame.addRoll(10) // 13
+
+    expect(newGame.total).to.equal(300)
+  })
+
+  it('should handle an entire game of spares', () => {
+    const newGame = new Game()
+
+    newGame.addRoll(5) // 1
+    newGame.addRoll(5)
+    newGame.addRoll(5) // 2
+    newGame.addRoll(5)
+    newGame.addRoll(5) // 3
+    newGame.addRoll(5)
+    newGame.addRoll(5) // 4
+    newGame.addRoll(5)
+    newGame.addRoll(5) // 5
+    newGame.addRoll(5)
+    newGame.addRoll(5) // 6
+    newGame.addRoll(5)
+    newGame.addRoll(5) // 7
+    newGame.addRoll(5)
+    newGame.addRoll(5) // 8
+    newGame.addRoll(5)
+    newGame.addRoll(5) // 9
+    newGame.addRoll(5)
+    newGame.addRoll(5) // 10
+    newGame.addRoll(5)
+    newGame.addRoll(5) // 11
+    newGame.addRoll(5)
+
+    expect(newGame.total).to.equal(150)
   })
 })
