632880c74cbda8277461ede78c6b845ac68c0017
Isaiah Santala <Isantala@yahoo.com>
2019-11-19 15:51:08 -0800

    added buttons to score for game

--- client/src/components/Score.js
+++ client/src/components/Score.js
@@ -2,22 +2,66 @@ import React, { useState } from 'react'
 
 const Score = ({ addRoll }) => {
 
-  const [score, setScore] = useState('')
-
   return (
-    <form onSubmit={e => {
-      e.preventDefault()
-      addRoll(score)
-      setScore('')
-    }}>
-      <input
-        value={score}
-        onChange={e => {
+    <form 
+      className="score-buttons"
+      onSubmit={e => {
+        e.preventDefault()
+        console.log(e.target)
+      }}
+    >
+      <div className="row">
+        <div className="score-button" onClick={e => {
           e.preventDefault()
-          setScore(e.target.value)
-        }}
-      ></input>
-      <button type="submit">submit</button>
+          addRoll(e.target.innerHTML)
+        }}>1</div>
+        <div className="score-button" onClick={e => {
+          e.preventDefault()
+          addRoll(e.target.innerHTML)
+        }}>2</div>
+        <div className="score-button" onClick={e => {
+          e.preventDefault()
+          addRoll(e.target.innerHTML)
+        }}>3</div>
+      </div>
+      <div className="row">
+        <div className="score-button" onClick={e => {
+          e.preventDefault()
+          addRoll(e.target.innerHTML)
+        }}>4</div>
+        <div className="score-button" onClick={e => {
+          e.preventDefault()
+          addRoll(e.target.innerHTML)
+        }}>5</div>
+        <div className="score-button" onClick={e => {
+          e.preventDefault()
+          addRoll(e.target.innerHTML)
+        }}>6</div>
+      </div>
+      <div className="row">
+        <div className="score-button" onClick={e => {
+          e.preventDefault()
+          addRoll(e.target.innerHTML)
+        }}>7</div>
+        <div className="score-button" onClick={e => {
+          e.preventDefault()
+          addRoll(e.target.innerHTML)
+        }}>8</div>
+        <div className="score-button" onClick={e => {
+          e.preventDefault()
+          addRoll(e.target.innerHTML)
+        }}>9</div>
+      </div>
+      <div className="row">
+        <div className="score-button" onClick={e => {
+          e.preventDefault()
+          addRoll(e.target.innerHTML)
+        }}>0</div>
+        <div className="score-button" onClick={e => {
+          e.preventDefault()
+          addRoll(e.target.innerHTML)
+        }}>10</div>
+      </div>
     </form>
   )
 }
